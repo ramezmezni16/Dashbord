b14a986fb36aa74f66d2906d8e7c66fbe5b1c009
ramezmezni16 <ramez.nabila@gmail.com>
2024-10-06 23:49:58 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 
 
@@ -16,6 +20,7 @@ app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 app.use(morgan("common"))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 
 
