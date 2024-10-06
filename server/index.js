5b723bda94770840985ae25441fa81c280a842a2
ramezmezni16 <ramez.nabila@gmail.com>
2024-10-07 00:37:30 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -10,29 +10,23 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
-
-
 dotenv.config();
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
-app.use(morgan("common"))
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-
-
 app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
-app.use("management", managementRoutes);
-app.use("sales", salesRoutes);
-
-
+app.use("/management", managementRoutes); // Fixed missing "/"
+app.use("/sales", salesRoutes); // Fixed missing "/"
 
 const PORT = process.env.PORT || 9000;
-
-
-
-
+mongoose.connect(process.env.MONGO_URL, {
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
