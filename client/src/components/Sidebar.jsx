87e47ae7e59f2ba98c5461dd297a8ee4489c613c
ramezmezni16 <ramez.nabila@gmail.com>
2024-10-12 14:20:53 +0100

    commit

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+    Box,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Typography,
+    useTheme
+} from "@mui/material"
+
+function Sidebar() {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
